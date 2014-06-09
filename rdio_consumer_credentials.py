2fa9c767721f98b372dd3897edce27766878130b
mrkaiser <kaiserollofdoom@gmail.com>
2014-06-08 22:39:20 -0400

    init

---
+++ rdio_consumer_credentials.py
@@ -0,0 +1,9 @@
+# Copy this file to rdio_consumer_credentials.py and add your
+# OAuth keys to get the examples to work.
+#
+# You can get OAuth keys by signing up for a developer account at:
+# http://developer.rdio.com/
+RDIO_CONSUMER_KEY = '9hrbs5wuu87hrex2uztejwf8'
+RDIO_CONSUMER_SECRET = 'Btj2jJGQhd'
+
+RDIO_CREDENTIALS = (RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET)
---
+++ services.py
@@ -0,0 +1,50 @@
+__author__ = 'mrkaiser'
+
+
+from rdio import Rdio
+from rdio_consumer_credentials import RDIO_CREDENTIALS
+import logging
+from flask import Flask, session, redirect, make_response
+
+app = Flask(__name__)
+
+
+@app.route('/login')
+def login():
+    #clear all of our auth cookies
+    #begin auth
+    rdio = Rdio(RDIO_CREDENTIALS)
+    url = rdio.begin_authentication(callback_url='127.0.0.1:5000/callback')
+    redirect_to_rdio = redirect(url)
+    #save our request token in cookies
+    response = make_response(redirect_to_rdio)
+    response.set_cookie('rt', rdio.token[0], expires=60*60*24)
+    response.set_cookie('rts', rdio.token[1], expires=60*60*24)
+    #go to Rdio to authenticate the app
+    return response
+
+@app.route('/callback')
+def callback():
+    app.logger.info('yayyyyyy')
+    #get the state from cookies and the query string
+    request_token = request.cookies.get('rt')
+    request_token_secret = request.cookies.get('rts')
+    verifier = request.args['oauth_verifier']
+    #make sure we have everything we need
+    if request_token and request_token_secret and verifier:
+        rdio = Rdio(RDIO_CREDENTIALS, (request_token, request_token_secret))
+        rdio.complete_authentication(verifier)
+        redirect_to_home = redirect('/')
+        response = make_response(redirect_to_home)
+        response.set_cookie('at', rdio.token[0], expires=60*60*24*14)   # expires in two weeks
+        response.set_cookie('ats', rdio.token[1], expires=60*60*24*14)  # expires in two weeks
+        response.set_cookie('rt', '', expires=-1)
+        response.set_cookie('rts', '', expires=-1)
+        return response
+    else:
+        response_to_home = make_response(redirect('/'))
+        return response_to_home
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
